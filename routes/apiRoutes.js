617056d8b585aa85269e18e19fd5b717197be914
Taylor Zettel <tzettel@gmail.com>
2020-02-06 17:45:35 -0500

    changed apiRoutes.js to reflect new model names

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,49 +3,51 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all users
   app.get("/api/users", function(req, res) {
-    db.User.findAll({}).then(function(dbUsers) {
+    db.Profiles.findAll({}).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
   // Get all answers
-  app.get("/api/answers", function(req, res) {
-    db.Survey.findAll({}).then(function(dbSurvey) {
+  app.get("/api/surveys", function(req, res) {
+    db.Answers.findAll({}).then(function(dbSurvey) {
       res.json(dbSurvey);
     });
   });
 
   // Create a new user
   app.post("/api/users", function(req, res) {
-    db.User.create(req.body).then(function(dbUser) {
+    db.Profiles.create(req.body).then(function(dbUser) {
       res.json(dbUser);
     });
   });
 
   // Create a new survey
   app.post("/api/surveys", function(req, res) {
-    db.Survey.create(req.body).then(function(dbSurvey) {
+    db.Answers.create(req.body).then(function(dbSurvey) {
       res.json(dbSurvey);
     });
   });
 
   // Get user by id
   app.get("/api/users/:id", function(req, res) {
-    db.User.findAll({ where: { id: req.params.id } }).then(function(dbUser) {
+    db.Profiles.findAll({ where: { id: req.params.id } }).then(function(dbUser) {
       res.json(dbUser);
     });
   });
 
   // Get answers by id
-  app.get("/api/answers/:id", function(req, res) {
-    db.Survey.findAll({ where: { id: req.params.id } }).then(function(dbSurvey) {
+  app.get("/api/surveys/:id", function(req, res) {
+    // eslint-disable-next-line prettier/prettier
+    db.Answers.findAll({ where: { id: req.params.id } }).then(function(dbSurvey) {
       res.json(dbSurvey);
     });
   });
 
   // Get connections by id
   app.get("/api/connections/:id", function(req, res) {
-    db.Survey.findAll({ where: { id: req.params.id } }).then(function(dbConnections) {
+    // eslint-disable-next-line prettier/prettier
+    db.Connections.findAll({ where: { id: req.params.id } }).then(function(dbConnections) {
       res.json(dbConnections);
     });
   });
