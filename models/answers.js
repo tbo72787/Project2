c9be55376e672e476dd628bceec1a5c1cc1c18a9
Taylor Zettel <tzettel@gmail.com>
2020-02-07 23:53:46 -0500

    fixing merge some more

--- models/answers.js
+++ models/answers.js
@@ -10,16 +10,5 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-<<<<<<< HEAD
-=======
-  Answers.associate = function(models) {
-    Answers.belongsTo(models.Answers, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
-
->>>>>>> 939a8cab08f8641c00433f295cb7bd64d87e2320
   return Answers;
 };
